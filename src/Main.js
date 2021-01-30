10b70bef7755712b5c4839af72fdd5d37039b917
ueken <62203822+hisoka0620@users.noreply.github.com>
2021-01-30 21:52:13 +0900

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <div className="main-wrapper">
+                <div className="container">
+                    <h1　className="fadeIn">ようこそ！私のホームページへ</h1>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Main;
